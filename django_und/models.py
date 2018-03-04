10d5a0b2acc685a3900753c801eefe498850175f
luxcem <a@luxcem.fr>
2018-03-04 15:14:30 +0100

    Fixes some docstrings.

--- django_und/models.py
+++ django_und/models.py
@@ -1,3 +1,4 @@
+"""Vote and VoteMixin."""
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.fields import GenericRelation
@@ -9,25 +10,21 @@ from django.utils.translation import ugettext_lazy as _
 
 class Vote(models.Model):
     """
-    Up and down vote model
+    Up and down vote model.
+
     By default this app use a foreignkey to a django user
     it also provide a method to use a username instead of a django
     user this allows the model to be used with various authentification
-    policies
+    policies.
     """
-    #: user
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True,
         related_name="und_votes"
     )
-    #: username
     username = models.CharField(max_length=255, null=True)
-    #: Score of the vote
     score = models.IntegerField(default=1)
-    # Django generic relation
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
-    #: The Voted object
     content_object = GenericForeignKey("content_type", "object_id")
 
     class Meta:
@@ -35,6 +32,7 @@ class Vote(models.Model):
         verbose_name_plural = _("Votes")
 
     def __str__(self):
+        """Return user, object, and score."""
         if getattr(settings, "UND_USE_USERNAME", False):
             return "{}:{}:{}".format(
                 self.username, self.content_object, self.score)
@@ -43,6 +41,11 @@ class Vote(models.Model):
 
     @classmethod
     def vote_with_user_object(cls, content_object, user_object, score):
+        """
+        Return a Vote instance.
+
+        Return a Vote instance for a user, a voted object and a score.
+        """
         if getattr(settings, "UND_USE_USERNAME", False):
             return Vote(
                 content_object=content_object,
@@ -52,14 +55,9 @@ class Vote(models.Model):
 
 
 class VoteMixin(models.Model):
-    """
-    A mixin to attach to a model that has up and down votes
-    """
-    #: Votes
+    """Mixin to attach to a model that has up and down votes."""
     und_votes = GenericRelation(Vote)
-    #: Total score of upvotes (is positive)
     und_score_up = models.IntegerField(default=0)
-    #: Total score of downvotes (is negative)
     und_score_down = models.IntegerField(default=0)
 
     class Meta:
@@ -67,16 +65,17 @@ class VoteMixin(models.Model):
 
     @property
     def und_score(self):
-        """Return the model score"""
+        """Return the model score."""
         return self.und_score_up + self.und_score_down
 
     def _get_with_object(self, user_object):
-        """Return a vote based on object and settings (UND_USE_USERNAME) """
+        """Return a vote based on object and settings (UND_USE_USERNAME)."""
         if getattr(settings, "UND_USE_USERNAME", False):
             return self.und_votes.get(username=user_object)
         return self.und_votes.get(user=user_object)
 
     def upvote(self, user_object):
+        """Upvote the instance with provided user."""
         # Change of score_up
         diff_up = 0
         # Change of score down
@@ -113,6 +112,7 @@ class VoteMixin(models.Model):
         )
 
     def downvote(self, user_object):
+        """Downvote the instance with the provided user."""
         # Change of score_up
         diff_up = 0
         # Change of score down
@@ -149,7 +149,7 @@ class VoteMixin(models.Model):
         )
 
     def reset_und_score(self):
-        """Reset score to the correct count (should not be necessary)"""
+        """Reset score to the correct count (should not be necessary)."""
         score_up = self.und_votes.filter(
             score__gt=0).aggregate(Sum('score'))['score__sum'] or 0
         score_down = self.und_votes.filter(
